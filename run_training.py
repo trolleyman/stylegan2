9d2fdf5eb183f7a724850b3137083ee07324996e
Callum Tolley <cgtrolley@gmail.com>
2020-06-21 13:48:17 +0100

    Change default snapshot ticks

--- run_training.py
+++ run_training.py
@@ -135,7 +135,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.data_dir = data_dir
     train.total_kimg = total_kimg
     train.mirror_augment = mirror_augment
-    train.image_snapshot_ticks = train.network_snapshot_ticks = 10
+    train.image_snapshot_ticks = train.network_snapshot_ticks = 1
     sched.G_lrate_base = sched.D_lrate_base = 0.002
     sched.minibatch_size_base = 32
     sched.minibatch_gpu_base = 4
